a57413367c6ff11778631ae25d713893f7d1659c
GShir97 <goldshir97@gmail.com>
2024-03-31 19:39:15 +0300

    WIP

---
+++ app/src/main/java/com/example/finalproject/fragments/signInFragment.java
@@ -0,0 +1,96 @@
+package com.example.finalproject.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.navigation.Navigation;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.finalproject.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.AuthResult;
+
+
+public class signInFragment extends Fragment {
+
+    private FirebaseAuth mAuth;
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public signInFragment() {
+        // Required empty public constructor
+    }
+
+    public static signInFragment newInstance(String param1, String param2) {
+        signInFragment fragment = new signInFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sign_in, container, false);
+
+        Button signInButton= view.findViewById(R.id.buttonSignup);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_signInFragment_to_signUpFragment);
+            }
+        });
+        return view;
+    }
+
+    public void loginFunc(View view) {
+
+        String email = ((EditText)view.findViewById(R.id.textEmailAddress)).getText().toString();
+        String password = ((EditText)view.findViewById(R.id.textPassword)).getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(getContext(), "Signing in successful.", Toast.LENGTH_LONG).show();
+                            Navigation.findNavController(view).navigate(R.id.action_signInFragment_to_homeFragment);
+                        } else {
+                            Toast.makeText(getContext(), "Signing in failed.", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/finalproject/fragments/signUpFragment.java
@@ -0,0 +1,83 @@
+package com.example.finalproject.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.finalproject.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class signUpFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private FirebaseAuth mAuth;
+    private String mParam1;
+    private String mParam2;
+
+    public signUpFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static signUpFragment newInstance(String param1, String param2) {
+        signUpFragment fragment = new signUpFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sign_in, container, false);
+        return view;
+    }
+
+        public void signupFunc(View view) {
+
+            String email = ((EditText)view.findViewById(R.id.emailSignup)).getText().toString();
+            String password = ((EditText)view.findViewById(R.id.passwordSignup)).getText().toString();
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Toast.makeText(getContext(), "Signing up successful.", Toast.LENGTH_LONG).show();
+                            } else {
+
+                                Toast.makeText(getContext(), "Authentication failed.", Toast.LENGTH_LONG).show();
+
+                            }
+                        }
+                    });
+        }
+    }
